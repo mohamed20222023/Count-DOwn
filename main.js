86db5e378b51ef87f5e50edb4b9436dcc58cebaa
mohamed20222023 <71554599+mohamed20222023@users.noreply.github.com>
2021-05-10 14:09:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ function show () {
         let name = document.getElementById('main').value ;
         let dat = document.getElementById('datetime').value ;
         let countDown = new Date(`${dat}`).getTime() ;
+      if(dat == "" || name == ""){
+          alert (`Enter Event's information`);
+      }else{
        x = setInterval(function(){
         let now = new Date();
         let resul = countDown - now ;
@@ -13,18 +16,22 @@ function show () {
         hour=  Math.floor((resul % (days)) / (hours)),
         min = Math.floor((resul % (hours)) / (minutes)),
         sec =  Math.floor((resul % (minutes)) / second);
+        if(hour < 0 || min < 0 || sec < 0 ){
+          alert(`please Enter a Date bigger than  ${now.toLocaleDateString()}`)
+        } else{
         document.getElementById("h1").innerHTML = name;
         document.getElementById("days").innerHTML = day < 10 ? `0${day}` : day ,
         document.getElementById("hours").innerHTML = hour < 10 ? `0${hour}` : hour,
         document.getElementById("minutes").innerHTML = min < 10 ? `0${min}` : min,
         document.getElementById("seconds").innerHTML = sec < 10 ? `0${sec}` : sec;
+      }
         if (resul < 0) {
             clearInterval(x);
           }
        }, 1000)
        document.getElementById('main').value = ""   ;
       }   
-
+  }
 document.getElementById("btn").addEventListener("click", function(event){
     event.preventDefault()
   });
