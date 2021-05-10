a3686d11039f78c9686896e9645937488725439a
Mohamed Eid <m01023598749@gmail.com>
2021-05-10 13:47:38 +0200

    Count Down

---
+++ main.js
@@ -0,0 +1,30 @@
+function show () { 
+        let   second = 1000 , 
+            minutes = second * 60 , 
+            hours = minutes * 60 , 
+            days = hours * 24 ;
+        let name = document.getElementById('main').value ;
+        let dat = document.getElementById('datetime').value ;
+        let countDown = new Date(`${dat}`).getTime() ;
+       x = setInterval(function(){
+        let now = new Date();
+        let resul = countDown - now ;
+        let day = Math.floor(resul / (days)),
+        hour=  Math.floor((resul % (days)) / (hours)),
+        min = Math.floor((resul % (hours)) / (minutes)),
+        sec =  Math.floor((resul % (minutes)) / second);
+        document.getElementById("h1").innerHTML = name;
+        document.getElementById("days").innerHTML = day < 10 ? `0${day}` : day ,
+        document.getElementById("hours").innerHTML = hour < 10 ? `0${hour}` : hour,
+        document.getElementById("minutes").innerHTML = min < 10 ? `0${min}` : min,
+        document.getElementById("seconds").innerHTML = sec < 10 ? `0${sec}` : sec;
+        if (resul < 0) {
+            clearInterval(x);
+          }
+       }, 1000)
+       document.getElementById('main').value = ""   ;
+      }   
+
+document.getElementById("btn").addEventListener("click", function(event){
+    event.preventDefault()
+  });
